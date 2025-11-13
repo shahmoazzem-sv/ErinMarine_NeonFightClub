0edf1a7d23ec4119dec9d1fb5a66d17003c93303
Tar Softvence <tar.softvence@gmail.com>
2025-11-13 15:19:13 +0600

    Package import from main branch
    Package imported for Tanvir from main branch to newly created Main2 branch

--- Assets/Tanvir/Scripts/AudioManager.cs
+++ Assets/Tanvir/Scripts/AudioManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance { get; private set; }
+
     [Header("Audio Source")]
-    [SerializeField] AudioSource musicSource;
-    [SerializeField] AudioSource sfxSource;
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
 
     [Header("Audio Clips")]
     public AudioClip menuBackground;
@@ -16,14 +21,67 @@ public class AudioManager : MonoBehaviour
     public AudioClip cardSelected;
     public AudioClip cardUnselected;
 
-    private void Start()
+    private void Awake()
     {
-        musicSource.clip = menuBackground;
-        musicSource.Play();
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            Transform music = transform.Find("Music");
+            if (music != null)
+                musicSource = music.GetComponent<AudioSource>();
+
+            Transform sfx = transform.Find("SFX");
+
+            if (sfx != null)
+                sfxSource = sfx.GetComponent<AudioSource>();
+        }
     }
 
+    private void Start()
+    {
+        if (musicSource != null && menuBackground != null)
+        {
+            musicSource.clip = menuBackground;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+    
+    public void SetSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (musicSource != null && clip != null)
+        {
+            musicSource.Stop();
+            musicSource.clip = clip;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+    }
     public void PlaySFX(AudioClip clip)
     {
-        sfxSource.PlayOneShot(clip);
+        if (sfxSource != null && clip != null)
+            sfxSource.PlayOneShot(clip);
     }
 }
